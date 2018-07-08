2b8ad6d515d5d986b9c31e3003b8059fe0bb8965
jmpark6846 <jmpark6846@gmail.com>
2018-07-08 17:06:44 +0900

    toggle button을 위한 state 추가

--- src/components/ui/NavigationBar.js
+++ src/components/ui/NavigationBar.js
@@ -5,18 +5,33 @@ import NavHeader from '../ui-components/NavBar/NavHeader';
 import NavMenu from '../ui-components/NavBar/NavMenu';
 import NavItem from '../ui-components/NavBar/NavItem';
 import './NavigationBar.css'
+import Icon from '../ui-components/Icon/Icon';
+import ToggleButton from '../ui-components/NavBar/ToggleButton';
 
-const NavigationBar = () => {
-  return (
-    <NavBar className='navigation-bar' >
-      <NavHeader>Simpleshop</NavHeader>
-      <NavMenu pullRight>
-        <NavItem to='/shop'>Shop</NavItem>
-        <NavItem to='/blog'>Blog</NavItem>
-        <NavItem to='/about'>About Us</NavItem>
-      </NavMenu>
-    </NavBar>
-  )
+class NavigationBar extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state={ toggle:false }
+  }
+
+  handleToggle = () => {
+    this.setState({toggle:!this.state.toggle})
+  }
+  
+  render(){
+    return (
+      <NavBar className='navigation-bar' >
+        <ToggleButton onClick={this.handleToggle}/>
+        <NavHeader>Simpleshop</NavHeader>
+        <NavMenu pullRight>
+          <NavItem to='/shop'>Shop</NavItem>
+          <NavItem to='/blog'>Blog</NavItem>
+          <NavItem to='/about'>About Us</NavItem>
+        </NavMenu>
+      </NavBar>
+    )
+  }
 }
 
 NavigationBar.propTypes = {
