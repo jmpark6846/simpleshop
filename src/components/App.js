8582058a26338b37c8bd1b3de0b9c0718d45bcac
jmpark6846 <jmpark6846@gmail.com>
2018-07-08 11:25:42 +0900

    submodule 다시 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
-import img from './img.png'
 
 export default class App extends Component {
   render() {
     return (
       <div className='App'>
-        simpleshop v1.0
-        <img src={img} />
+        <NavBar />
+        
       </div>
     )
   }
