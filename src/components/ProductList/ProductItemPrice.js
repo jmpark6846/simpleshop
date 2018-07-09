40c9da22297f5a10e3cfe8c8b75293870be1a7b0
jmpark6846 <jmpark6846@gmail.com>
2018-07-09 20:13:22 +0900

    productlist 중 공통 컴포넌트들로 대체

--- src/components/ProductList/ProductItemPrice.js
+++ src/components/ProductList/ProductItemPrice.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const ProductItemPrice = ({price, suffix}) => {
-  console.log(String(price).length)
   const formattedPrice = String(price).split('').reverse().map((c,i)=>(i+1)%3 === 0 && i !== String(price).length-1 ? ','+c : c).reverse().join('')
   return (
     <div className='product-price'>{formattedPrice}{suffix}</div>
--- src/components/ProductList/ProductListItem.js
+++ src/components/ProductList/ProductListItem.js
@@ -2,15 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './ProductListItem.css'
 import ProductItemName from './ProductItemName';
-import ProductItemPrice from './ProductItemPrice';
-import ProductItemImg from './ProductItemImg';
+import Image from '../ui-components/Image'
 
 const ProductListItem = ({product}) => {
   return (
     <div className='product-list-item'>
-      <ProductItemImg img={product.img} />
+      <Image img={product.img} className='product-img' />
       <ProductItemName name={product.name} /> 
-      <ProductItemPrice suffix='원' price={product.price} />
+      <Price className='product-price' suffix='원' price={product.price} />
     </div>
   )
 }
