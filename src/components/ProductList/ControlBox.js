ff5787c8bd7b0aca7a09f17086bc2a309a5e07ad
jmpark6846 <jmpark6846@gmail.com>
2018-07-09 17:33:17 +0900

    control box 추가

---
+++ src/components/ProductList/ControlBox.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ControlBox = () => {
+  return (
+    <div className='control-box'>
+      
+    </div>
+  )
+}
+
+ControlBox.propTypes = {
+
+}
+
+export default ControlBox
