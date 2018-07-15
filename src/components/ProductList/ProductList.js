197155d070c01695492373bff19718fef4d4e737
jmpark6846 <jmpark6846@gmail.com>
2018-07-15 17:46:48 +0900

    프로덕트에 장바구니 추가 버튼 추가

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -2,18 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Container, Row, Col } from '../ui-components/grid';
 import ProductListItem from './ProductListItem';
-
-const products = [
-  { id:0, name: 'Adventurist Daypack - Black', price: 590000, img: '/src/asset/img/2.jpg' },  
-  { id:1, name: 'Adventurist Daypack - Pine', price: 5800000, img: '/src/asset/img/1.jpeg' },   
-  { id:2, name: 'Adventurist Daypack - Sand', price: 60000, img: '/src/asset/img/3.jpg' },
-  { id:3, name: 'Adventurist Daypack - Black', price: 610, img: '/src/asset/img/2.jpg' },  
-  { id:4, name: 'Adventurist Daypack - Pine', price: 6200, img: '/src/asset/img/1.jpeg' },   
-  { id:5, name: 'Adventurist Daypack - Sand', price: 6300000, img: '/src/asset/img/3.jpg' },
-  { id:6, name: 'Adventurist Daypack - Black', price: 6400, img: '/src/asset/img/2.jpg' },  
-  { id:7, name: 'Adventurist Daypack - Pine', price: 65000, img: '/src/asset/img/1.jpeg' },   
-  { id:8, name: 'Adventurist Daypack - Sand', price: 66000, img: '/src/asset/img/3.jpg' }
-]
+import { products } from "../../constants/dummy";
 
 const ProductList = () => {
   return (
--- src/components/ProductList/ProductListItem.js
+++ src/components/ProductList/ProductListItem.js
@@ -1,25 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import ProductItemName from './ProductItemName';
+import { connect } from "react-redux";
 import Image from '../ui-components/Image'
 import Price from "../Price";
 import { NavLink } from "react-router-dom";
 import './ProductListItem.css'
+import Button from '../ui/Button';
+import { doAddToCart } from '../../actions/cart';
 
-const ProductListItem = ({product}) => {
+const ProductListItem = ({product, addToCart}) => {
+  const cartItem = {id: product.id, name: product.name, price: product.price, img: product.imgs[0], ea: 1}
   return (
-    <NavLink to={`/product/${product.id}`}>
-      <div className='product-list-item'>
-        <Image src={product.img} className='product-img' />
+    <div className='product-list-item'>
+      <NavLink to={`/product/${product.id}`}>
+        <div className='product-img' style={{backgroundImage: `url(${product.imgs[0]})`}}></div>
         <ProductItemName name={product.name} /> 
         <Price className='product-price' suffix='원' price={product.price} />
-      </div>
-    </NavLink>
+      </NavLink>
+      <Button icon='fas fa-shopping-cart' className='shortcut-cart' onClick={()=>addToCart(cartItem)}/>
+    </div>
   )
 }
 
 ProductListItem.propTypes = {
-  product: PropTypes.object
+  product: PropTypes.object,
+  addToCart: PropTypes.func,
 }
 
-export default ProductListItem
+const mapDispatch = (dispatch) => ({
+  addToCart: (cartItem) => dispatch(doAddToCart(cartItem))
+})
+
+export default connect(undefined,mapDispatch)(ProductListItem)
