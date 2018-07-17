b58e1a160eb9bfbdb1c4c6601e54a290d004f83e
jmpark6846 <jmpark6846@gmail.com>
2018-07-17 18:08:46 +0900

    product action type 추가

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,4 +1,12 @@
+// cart
 export const ADD_TO_CART = 'ADD_TO_CART'
 export const TOGGLE_CART = 'TOGGLE_CART'
 export const DELETE_CART_ITEM = 'DELETE_CART_ITEM'
 export const LOAD_NEW_PAGE = 'LOAD_NEW_PAGE'
+
+// product
+export const PRODUCT_LOAD = 'PRODUCT_LOAD'
+export const REVIEW_ADD = 'REVIEW_ADD'
+export const REVIEW_DELETE = 'REVIEW_DELETE'
+
+
