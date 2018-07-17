a6f392456997f758f4a3ead803bee86dfed3cd81
jmpark6846 <jmpark6846@gmail.com>
2018-07-17 18:17:31 +0900

    page 로드 시 마다 실행되는 load()를 componentDidMount 로 이동

--- src/page/PageLayout.js
+++ src/page/PageLayout.js
@@ -4,17 +4,21 @@ import PropTypes from 'prop-types'
 import NavigationBar from '../components/ui/NavigationBar'
 import { doLoadNewPage } from '../actions/cart';
 
-const PageLayout = ({page, children, load}) => {
-  load()
-  return (
-    <React.Fragment>
-      <NavigationBar />
-      <div className={`page ${page}`}>
-        {children}
-      </div>
-      
-    </React.Fragment>
-  )
+class PageLayout extends React.Component{
+  componentDidMount = () => {
+    this.props.load()
+  }
+  render(){
+    const { page, children } = this.props
+    return (
+      <React.Fragment>
+        <NavigationBar />
+        <div className={`page ${page}`}>
+          {children}
+        </div>
+      </React.Fragment>
+    )
+  }
 }
 
 PageLayout.propTypes = {
