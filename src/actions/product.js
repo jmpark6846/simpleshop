69bf9ba11706e3b9ba870a5f974ac68e47a19f55
jmpark6846 <jmpark6846@gmail.com>
2018-07-18 15:30:35 +0900

    review 추가 삭제 액션 크리에이터 추가

--- src/actions/product.js
+++ src/actions/product.js
@@ -1,6 +1,16 @@
-import { PRODUCT_LOAD } from "../constants/actionTypes";
+import { PRODUCT_LOAD, REVIEW_ADD, REVIEW_DELETE } from "../constants/actionTypes";
 
 export const doLoadProduct = (product) => ({
   type: PRODUCT_LOAD,
   product
+})
+
+export const doReviewAdd = (review) => ({
+  type: REVIEW_ADD,
+  review
+})
+
+export const doReviewDelete = (id) => ({
+  type: REVIEW_DELETE,
+  id
 })
