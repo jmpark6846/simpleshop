041e8652a70396eca899bbbe3bcb4a426124939e
jmpark6846 <jmpark6846@gmail.com>
2018-07-19 14:46:38 +0900

    cart 내에 버튼, 가격에 cartItems 컴포넌트 대신 div.cart-item-wrapper로 변경

--- src/components/ui/Cart/Cart.js
+++ src/components/ui/Cart/Cart.js
@@ -12,22 +12,22 @@ import Price from '../../ui-components/Price/Price';
 export const Cart = ({cartItems, show, error, errorMsg, totalPrice}) => {
   return (
       <div className={classNames('cart', { show } )}>
-        { Object.keys(cartItems).length !== 0 ? 
+        { cartItems &&Object.keys(cartItems).length !== 0 ? 
           Object.keys(cartItems).map(id=> <CartItem key={id} cartItem={cartItems[id]} />)          
           :
-          <div className='cart-item'>장바구니가 비었습니다.</div> 
+          <div className='cart-item-wrapper cart-empty-message'>장바구니가 비었습니다.</div> 
         }
         { error && 
-          <CartItem className='error'>
+          <div className='cart-item-wrapper error'>
             <Icon icon='fas fa-info-circle' />
             {errorMsg}
-          </CartItem> }
-        <CartItem>
+          </div> }
+        <div className='cart-item-wrapper'>
           총 금액: <Price className='cart-total-price' price={totalPrice} suffix='원'/>
-        </CartItem> 
-        <CartItem>
+        </div> 
+        <div className='cart-item-wrapper'>
           <Button className='buy' value='구매하기' primary/>
-        </CartItem>
+        </div>
       </div>
   )
 }
