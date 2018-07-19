3b1e2da28e0d582c2feb8dd48a38c043f9c899f4
jmpark6846 <jmpark6846@gmail.com>
2018-07-19 17:18:48 +0900

    카트에서 에러 표현 div 삭제; 카트 버튼을 카트 내부로 들고옴

--- src/components/ui/Cart/Cart.js
+++ src/components/ui/Cart/Cart.js
@@ -9,19 +9,19 @@ import Icon from '../../ui-components/Icon/Icon';
 import Button from '../../ui-components/Button/Button';
 import Price from '../../ui-components/Price/Price';
 
-export const Cart = ({cartItems, show, error, errorMsg, totalPrice}) => {
+export const Cart = ({show, cartItems, totalPrice}) => {
   return (
-      <div className={classNames('cart', { show } )}>
+    <div className='cart-wrapper'>
+      <Button className='shopping-cart' icon='fas fa-shopping-cart'/>
+      <div className={classNames('cart', { show })} >
+        
+
         { cartItems &&Object.keys(cartItems).length !== 0 ? 
           Object.keys(cartItems).map(id=> <CartItem key={id} cartItem={cartItems[id]} />)          
           :
           <div className='cart-item-wrapper cart-empty-message'>장바구니가 비었습니다.</div> 
         }
-        { error && 
-          <div className='cart-item-wrapper error'>
-            <Icon icon='fas fa-info-circle' />
-            {errorMsg}
-          </div> }
+      
         <div className='cart-item-wrapper'>
           총 금액: <Price className='cart-total-price' price={totalPrice} suffix='원'/>
         </div> 
@@ -29,22 +29,18 @@ export const Cart = ({cartItems, show, error, errorMsg, totalPrice}) => {
           <Button className='buy' value='구매하기' primary/>
         </div>
       </div>
+    </div>
   )
 }
 
 Cart.propTypes = {
-  cartItems: PropTypes.object,
   show: PropTypes.bool,
-  error: PropTypes.bool,
-  errorMsg: PropTypes.string,
+  cartItems: PropTypes.object,
   totalPrice: PropTypes.number
 }
 
 const mapState = ({cart}) => ({
   cartItems: cart.cartItems,
-  show: cart.show,
-  error: cart.error,
-  errorMsg: cart.errorMsg,
   totalPrice: cart.totalPrice
 })
 
