8f06ec3412cb307ab50795f2949330654fadd277
jmpark6846 <jmpark6846@gmail.com>
2018-07-20 18:28:22 +0900

    카트 ui 수정

--- src/components/ui/Cart/Cart.js
+++ src/components/ui/Cart/Cart.js
@@ -9,26 +9,31 @@ import Icon from '../../ui-components/Icon/Icon';
 import Button from '../../ui-components/Button/Button';
 import Price from '../../ui-components/Price/Price';
 
-export const Cart = ({show, cartItems, totalPrice}) => {
+export const Cart = ({show, cartItems, totalPrice, AddOrder}) => {
   return (
     <div className='cart-wrapper'>
       <Button className='shopping-cart' icon='fas fa-shopping-cart'/>
       <div className='cart-preview-div'>
         <i className='chat-start-icon'></i>
-        <div className={classNames('cart', { show })} >
-          { cartItems &&Object.keys(cartItems).length !== 0 ? 
-            Object.keys(cartItems).map(id=> <CartItem key={id} cartItem={cartItems[id]} />)          
+        
+          { cartItems && Object.keys(cartItems).length !== 0 ? 
+            <div className={classNames('cart', { show })} >
+              <div>
+                { Object.keys(cartItems).map(id=> <CartItem key={id} cartItem={cartItems[id]} />) }
+              </div>
+              <div className='cart-item-wrapper'>
+                총 금액: <Price className='cart-total-price' price={totalPrice} suffix='원'/>
+              </div> 
+              <div className='cart-item-wrapper'>
+                <Button className='buy' value='구매하기' onClick={()=>AddOrder(cartItems)} primary/>
+              </div>
+            </div>
             :
-            <div className='cart-item-wrapper cart-empty-message'>장바구니가 비었습니다.</div> 
+            <div className={classNames('cart', { show })} >
+              <div className='cart-item-wrapper cart-empty-message'>장바구니가 비었습니다.</div>  
+            </div>
           }
         
-          <div className='cart-item-wrapper'>
-            총 금액: <Price className='cart-total-price' price={totalPrice} suffix='원'/>
-          </div> 
-          <div className='cart-item-wrapper'>
-            <Button className='buy' value='구매하기' primary/>
-          </div>
-        </div>
       </div>  
     </div>
   )
@@ -45,4 +50,8 @@ const mapState = ({cart}) => ({
   totalPrice: cart.totalPrice
 })
 
-export default connect(mapState)(Cart)
+const mapDispatch = (dispatch) => ({
+  AddOrder: orderItems => dispatch(doOrderAdd(orderItems))
+})
+
+export default connect(mapState, mapDispatch)(Cart)
