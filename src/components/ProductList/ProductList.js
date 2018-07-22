459530464113c2486b7d5cac4ea76e38343bfd08
jmpark6846 <jmpark6846@gmail.com>
2018-07-22 19:22:04 +0900

    redux store와 ProductList 연결

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,21 +1,27 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Container, Row, Col } from '../ui-components/grid';
 import ProductListItem from './ProductListItem';
-import { products } from "../../constants/dummy";
 
-const ProductList = () => {
+const ProductList = ({productList}) => {
   return (
     <div className='product-list'>
-      <Container>
-        <Row>
-          { products.map((p,i)=>
-            <Col key={i} desktop={4}><ProductListItem product={p} /></Col>)
-          }
-        </Row>
-      </Container>
+      { productList ?
+        <Container>
+          <Row>
+            { Object.values(productList).map((p,i)=>
+              <Col key={i} desktop={4}><ProductListItem product={p} /></Col>)
+            }
+          </Row>
+        </Container>
+        :
+        <Container>Loading...</Container>
+      }
     </div>
   )
 }
 
-
-export default ProductList
+const mapState = ({product}) => ({
+  productList: product.productList
+})
+export default connect(mapState)(ProductList)
