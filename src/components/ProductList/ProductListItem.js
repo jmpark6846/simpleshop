4a02d4c27affce50beb255ccfb931e0dc0b51717
jmpark6846 <jmpark6846@gmail.com>
2018-07-23 18:23:57 +0900

    ProductListItem에 CartMessage 추가

--- src/components/ProductList/ProductListItem.js
+++ src/components/ProductList/ProductListItem.js
@@ -7,19 +7,44 @@ import { NavLink } from "react-router-dom";
 import './ProductListItem.css'
 import Button from '../ui-components/Button/Button';
 import { doAddToCart } from '../../actions/cart';
+import CartMessage from '../ui/Cart/CartMessage';
 
-export const ProductListItem = ({product, addToCart}) => {
-  const cartItem = {id: product.id, name: product.name, price: product.price, img: product.imgs[0], ea: 1}
-  return (
-    <div className='product-list-item'>
-      <NavLink to={`/product/${product.id}`}>
-        <div className='product-img' style={{backgroundImage: `url(${product.imgs[0]})`}}></div>
-        <ProductItemName name={product.name} /> 
-        <Price className='product-price' suffix='원' price={product.price} />
-      </NavLink>
-      <Button icon='fas fa-shopping-cart' className='shortcut-cart' onClick={()=>addToCart(cartItem)}/>
-    </div>
-  )
+class ProductListItem extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state = { 
+      cartMessageShow: false
+    }
+  }
+
+  onMessageDismiss = () => {
+    this.setState({ cartMessageShow: false })
+  }
+
+  handleAddToCart = () => {
+    this.props.addToCart(this.cartItem)
+    this.setState({ cartMessageShow: true })
+  }
+  get cartItem(){
+    const { product } = this.props
+    return {id: product.id, name: product.name, price: product.price, img: product.imgs[0], ea: 1}
+  }
+  render(){
+    const { cartMessageShow } = this.state
+    const {product} = this.props
+    return (
+      <div className='product-list-item'>
+        <NavLink to={`/product/${product.id}`}>
+          <div className='product-img' style={{backgroundImage: `url(${product.imgs[0]})`}}></div>
+          <ProductItemName name={product.name} /> 
+          <Price className='product-price' suffix='원' price={product.price} />
+        </NavLink>
+        <CartMessage onMessageDismiss={this.onMessageDismiss} show={cartMessageShow}/>
+        <Button icon='fas fa-shopping-cart' className='shortcut-cart' onClick={this.handleAddToCart}/>
+      </div>
+    )
+  }
 }
 
 ProductListItem.propTypes = {
