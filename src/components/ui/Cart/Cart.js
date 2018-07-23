f7744b692d2452eef253840b5592177bad753edc
jmpark6846 <jmpark6846@gmail.com>
2018-07-23 15:44:12 +0900

    cart에 history.push를 사용하기위해 withRouter 추가

--- src/components/ui/Cart/Cart.js
+++ src/components/ui/Cart/Cart.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from "react-redux";
+import { withRouter } from 'react-router-dom'
+
 import classNames from 'classnames'
 import './Cart.css'
 import CartItem from './CartItem';
 import Button from '../../ui-components/Button/Button';
 import Price from '../../ui-components/Price/Price';
-import { doOrderAdd } from '../../../actions/order';
-import { NavLink } from 'react-router-dom'
+import { doPurchase } from '../../../actions/order';
 
-export const Cart = ({show, cartItems, totalPrice, AddOrder}) => {
+export const Cart = ({show, cartItems, totalPrice, purchase, history}) => {
   return (
     <div className='cart-wrapper'>
       <Button className='shopping-cart' icon='fas fa-shopping-cart'/>
@@ -25,9 +26,7 @@ export const Cart = ({show, cartItems, totalPrice, AddOrder}) => {
                 총 금액: <Price className='cart-total-price' price={totalPrice} suffix='원'/>
               </div> 
               <div className='cart-item-wrapper'>
-                <NavLink to='/checkout'>
-                  <Button className='buy' value='구매하기' onClick={()=>AddOrder(cartItems)} primary/>
-                </NavLink>
+                <Button className='buy' value='구매하기' onClick={()=>purchase(cartItems, history.push)} primary/>
               </div>
             </div>
             :
@@ -53,7 +52,7 @@ const mapState = ({cart}) => ({
 })
 
 const mapDispatch = (dispatch) => ({
-  AddOrder: orderItems => dispatch(doOrderAdd(orderItems))
+  purchase: (orderItems, push) => dispatch(doPurchase(orderItems, push))
 })
 
-export default connect(mapState, mapDispatch)(Cart)
+export default withRouter(connect(mapState, mapDispatch)(Cart))
