c7f8a0250c0d118dfa07415602194dd9ebd07513
jmpark6846 <jmpark6846@gmail.com>
2018-07-23 15:42:46 +0900

    purchase 액션 크리에이터  추가

--- src/actions/order.js
+++ src/actions/order.js
@@ -1,6 +1,20 @@
-import { ORDER_ADD } from "../constants/actionTypes";
+import { PURCHASE, PLACE_ORDER, PURCHASE_SUCCESS, PURCHASE_FAIL } from "../constants/actionTypes";
 
-export const doOrderAdd = (orderItems) => ({
-  type: ORDER_ADD,
+export const doPurchase = (orderItems, push) => ({
+  type: PURCHASE,
+  orderItems,
+  push
+})
+
+export const doPlaceOrder = (orderItems) => ({
+  type: PLACE_ORDER,
   orderItems
+})
+
+export const doPurchaseSuccess = () => ({
+  type: PURCHASE_SUCCESS
+})
+
+export const doPurchaseFail = () => ({
+  type: PURCHASE_FAIL
 })
